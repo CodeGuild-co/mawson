30260acb4cd3acef7b0b3604db9f54a0e1358cdd
Matthew <mawson1998@icloud.com>
2015-11-25 13:45:40 -0500

    Added Billy's guest post

--- blog/blog.py
+++ blog/blog.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flaskext.markdown import Markdown
 
 app = Flask(__name__)
+Markdown(app)
 
 
 @app.route('/')
@@ -10,7 +12,7 @@ def home():
 
 @app.route('/posts/<name>/')
 def posts(name):
-    return render_template('posts/%s.html' % name)
+	return render_template('posts/%s.html' % name)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')

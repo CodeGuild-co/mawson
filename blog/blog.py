8044892c3bea5f7606c152cee91a4438eb89ec7b
Matthew <mawson1998@icloud.com>
2015-11-26 10:41:20 -0500

    added a write feature

--- blog/blog.py
+++ blog/blog.py
@@ -1,18 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flaskext.markdown import Markdown
 
 app = Flask(__name__)
 Markdown(app)
 
-
 @app.route('/')
 def home():
-    return render_template('home.html')
-
+    	return render_template('home.html')
 
 @app.route('/posts/<name>/')
 def posts(name):
 	return render_template('posts/%s.html' % name)
+
+@app.route('/write')
+def write():
+     return render_template('write/index.html', author = 'Matthew')
+
+@app.route('/write', methods=['POST'])
+def writepost():
+
+    texts = request.form['text']
+    with open('blog/templates/posts/output.txt', 'w') as f:
+        f.write('hello' + request.form['text'])
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')

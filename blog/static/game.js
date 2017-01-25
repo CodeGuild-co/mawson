5879de8f6eed2d252f4f62aed1c562f1b3638d94
Mawsonator <mawson1998@icloud.com>
2017-01-25 08:14:55 +0000

    Create game.js

---
+++ blog/static/game.js
@@ -0,0 +1,84 @@
+var c=document.getElementById("canvas");
+var ctx=c.getContext("2d");
+var enemynum = 0;
+var enemycounter=100;
+
+function draw(){
+ctx.clearRect(0, 0, 400, 400);
+player.draw();
+for(var i = 0; i<enemynum; i++)
+  {
+    enemies[i].y++;
+    enemies[i].draw();
+  }
+}
+
+
+var FPS = 30;
+canvas.addEventListener('mousemove', MouseMove, false);
+setInterval(function() {
+  update();
+  draw();
+}, 1000/FPS);
+
+
+var player = {
+  color: "#00A",
+  x: 220,
+  y: 270,
+  width: 32,
+  height: 32,
+  draw: function() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+};
+
+var enemies = [];
+
+function update(){
+  // Add only 4 enemies to the screen
+  if(enemynum<4)
+  {
+    enemycounter++;
+    if(enemycounter > 100)
+    // Spacing of enemies
+    {
+      enemycounter = 0;
+      enemynum++;
+    }  
+  }
+  
+  for(var i = 0; i<enemynum; i++)
+  {
+    enemies.push({
+    color: "#0F0",
+    x: 0,
+    y: -20,
+    width: 400,
+    height: 20,
+    draw: function() {
+      if(this.y>400)
+      {
+        this.y=-this.height;
+      }
+      ctx.fillStyle = this.color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+      }
+    })
+  }
+ 
+}
+
+var holes = [];
+
+
+
+
+
+function MouseMove(e){
+
+player.x = e.clientX+10;
+player.y = e.clientY+10; 
+
+}
